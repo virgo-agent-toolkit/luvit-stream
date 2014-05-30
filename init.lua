820abf840877f229d7d9bbea5ad0029d7f9534c0
Song Gao <song@gao.io>
2014-05-29 22:20:11 -0400

    add stream_writable

--- init.lua
+++ init.lua
@@ -1,13 +1,18 @@
-local stream_readable = require('./lib/stream_readable')
 local stream = require('./lib/stream')
+local stream_readable = require('./lib/stream_readable')
+local stream_writable = require('./lib/stream_writable')
 
 local exports = {}
+
+for k,v in pairs(stream) do
+  exports[k] = v
+end
 
 for k,v in pairs(stream_readable) do
   exports[k] = v
 end
 
-for k,v in pairs(stream) do
+for k,v in pairs(stream_writable) do
   exports[k] = v
 end
 
